1d9ca701ef6bc508987121e83b6d63afdc26fe83
Marcop977 <136688345+Marcop977@users.noreply.github.com>
2024-01-26 12:58:50 +0100

    second-commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,57 @@
 new Swiper("#swiper-1", {
-    effect: "fade",
+    effect: "cube",
     autoplay: {
         delay: 2500
     },
-    pagination: {
-        el: "#swiper-1 .swiper-pagination",
-        clickable: true
-    }
+    // pagination: {
+    //     el: "#swiper-1 .swiper-pagination",
+    //     clickable: true
+    // }
 });
 
-function myMap() {
-    var mapProp= {
-      center:new google.maps.LatLng(51.508742,-0.120850),
-      zoom:5,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    }
+const map = L.map('map').setView([45.4615149,9.1828855], 15);
+
+const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+osm.addTo(map);
+
+googleStreets = L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+googleStreets.addTo(map);
+
+let myIcon = L.icon({
+    iconUrl: './img/marker.png',
+    iconSize: [68, 65]
+});
+
+L.marker([45.4615149,9.1828855], { icon: myIcon }).addTo(map);
+
+
+
+// 2 swiper
+const swiper = new Swiper('#swiper-2', {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    virtual: {
+      slides: (function () {
+        const slides = [];
+        for (var i = 0; i < 600; i += 1) {
+          slides.push(`<a href=""><img class="" src="./img/slide2-${(i + 1)}.jpeg" alt=""></a>`);
+        }
+        return slides;
+      })(),
+    },
+});
+  
